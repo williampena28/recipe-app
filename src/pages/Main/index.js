0a2f6f858d09355cdf86ee82f645466e3d4cbbbc
williampena28 <56842242+williampena28@users.noreply.github.com>
2023-02-28 14:09:54 -0500

    render error resolve

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-// import DisplayRecipes from '../../components/display_recipes';
+import DisplayRecipes from '../../components/display_recipes/index.js';
 
 const MainPage = () => {
   return (
     <div>
       <h1>Recipe List:</h1>
-      {/* <DisplayRecipes/> */}
+      <DisplayRecipes/>
     </div>
   )
 }
